d0b18965290f7d7bc7615f35e7da8c0cd3fe64ca
SlemJosh <joshua.slem@gmail.com>
2023-09-24 17:51:21 -0600

    Added a script to get the current time and display it. The format however doesn't match the example, so through some additional research found a different method that is actually a lot shorter to add.

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,36 @@ $(function () {
 
   // Added the function saveTasks so that we can take what information is given and store it in the local storage.  Users can refresh or even leave the page and the info will stay there until cleared/we set a timer to auto delete it.
   // Formatting is just so that we can save it to the appropriate time slot.  As long as it's between our 9am and 5pm (17hour) we want to be able to save that data in addition to whatever they put into the text box.
-  function saveTasks()_{
+  function saveTasks(){
     for (i = 9; i <= 17; i++){
       localStorage.setItems("hour" + i, $("#hour" + i + "text").val());
     }
   }
 
 
-  
+
+
+  // We want to display the current date and time.  Our header has a class already tied to it, so we will just use that here in the function to pull the data from the users pc and display it on the screen.
+
+  let time = Date.now()
+  console.log(time);
+
+  var date = Date(Date.now());
+
+  let currentDate = date.toString();
+  console.log(currentDate);
+  document.getElementById("currentDay").innerHTML = currentDate;
+
+  //let today = dayjs().format("ddd, MM/DD/YYYY");
+  //document.getElementById("currentDay").innerHTML = today;
+
+// https://www.freecodecamp.org/news/javascript-date-time-dayjs/#:~:text=const%20currentDate%20%3D%20dayjs()%3B,That's%20it!
+
+  //let today = dayjs().format("ddd, MM/DD/YYYY");
+  //document.getElementById("currentDay").innerHTML = today;
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
