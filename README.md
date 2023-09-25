04c2bf52f7ca4e6029916681e7b26076fd49ca41
SlemJosh <joshua.slem@gmail.com>
2023-09-24 21:15:23 -0600

    Updated the Readme. Did some cleanup and formatting on the index and script pages.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# work-day-scheduler
+## Work-Day-Scheduler
+
+## Description
+
+
+## Installation
+Deployed at https://slemjosh.github.io/work-day-scheduler/
+
+## Usage
+![password generator webpage.](./assets/images/passwordgenerator1.png)
+
+
+## Credits
+
+
+
+## License
+
+Please refer to the LICENSE in the repo
+
--- assets/script.js
+++ assets/script.js
@@ -10,39 +10,39 @@ $(function () {
       localStorage.setItem("hour" + i, $("#hour" + i + "text").val());
     }
   }
-  
+
   // Now that we have that information being saved, we need a way to display it, outside of just being stored on the developer tools. So we use a function to pull that information and display it to the user.
-    for (i = 9; i <= 17; i++) {
+  for (i = 9; i <= 17; i++) {
     $("#hour" + i + "text").val(localStorage.getItem("hour" + i));
   }
 
   // Time to deal with the time coding. Our stylesheet already has it setup to display the colors for past, present, and future.  We just need to be able to tell the system how those hours relate to our current time.
   // We go into our container as specificed on the index.  And then for each time interval (children) and telling it to change the div class to our new values based on the time.
-$(".container-lg").children("div").each(timeColor);
+  $(".container-lg").children("div").each(timeColor);
 
-function timeColor(){
-  let currentHour = dayjs().format("H");
-  console.log(currentHour)
-  // If the current time is later than our listed times...
-  for (i = 9; i <= 17; i++){
-    if (i < currentHour) {
-      $("#hour-" + i).toggleClass("past");
-    }
-   // If the current hour is the current hour ...
-    if (i == currentHour) {
-      $("#hour-" + i).toggleClass("present");
-    }
-  // If the current hour is before the hours listed ...
-    if (i > currentHour) {
-      $("#hour-" + i).toggleClass("future");
+  function timeColor() {
+    let currentHour = dayjs().format("H");
+    console.log(currentHour)
+    // If the current time is later than our listed times...
+    for (i = 9; i <= 17; i++) {
+      if (i < currentHour) {
+        $("#hour-" + i).toggleClass("past");
+      }
+      // If the current hour is the current hour ...
+      if (i == currentHour) {
+        $("#hour-" + i).toggleClass("present");
+      }
+      // If the current hour is before the hours listed ...
+      if (i > currentHour) {
+        $("#hour-" + i).toggleClass("future");
+      }
     }
   }
-}
   // We want to display the current date and time.  Our header has a class already tied to it, so we will just use that here in the function to pull the data from the users pc and display it on the screen.
   // Used the dayjs() function.  And then we chose for it to be display in a format that will show us the abbreviation for the day, followed by the actual date in the format we provide.
   // Then we just take that information and apply it to the class id currentDay given to us on the index page.
 
-  let today = dayjs().format("dddd, MM/DD/YYYY");  //https://day.js.org/docs/en/display/format
+  let today = dayjs().format("dddd, MM/DD/YYYY, hh:mm");  //https://day.js.org/docs/en/display/format
   document.getElementById("currentDay").innerHTML = today;
 
   // reference for dayjs()
